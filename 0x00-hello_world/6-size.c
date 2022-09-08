af8656bcabd15337f25035f05c834a049f4a46b8
eniolakura <enybeats76@gmail.com>
2022-09-08 16:19:01 -0700

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
 	long long int ll;
 	float f;
 
-	
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(l));
