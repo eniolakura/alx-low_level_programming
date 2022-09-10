123bf8474853eec565cb3d09f6bf8751991a7d6b
eniolakura <enybeats76@gmail.com>
2022-09-10 12:29:04 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers in base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int i;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
