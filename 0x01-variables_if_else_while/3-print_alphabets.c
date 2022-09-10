a93359b55574c0344409f9514eaeb02594b3589d
eniolakura <enybeats76@gmail.com>
2022-09-09 17:14:42 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alpha[i]);
+	}
+	return (0)
+	putchar('\n');
+}
