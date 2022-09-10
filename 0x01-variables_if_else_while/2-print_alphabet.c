188576acfb8fc2b7185365233c9fec1cd060f1a1
eniolakura <enybeats76@gmail.com>
2022-09-09 17:05:53 -0700

    changes

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 /**
- * mainn - Prints alphabets
+ * main - Prints alphabets
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alpha[26] = "abcdefghijklmnoprstuvwxyz";
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0 ; i < 26; i++)
