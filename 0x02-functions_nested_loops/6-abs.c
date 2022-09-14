c64dc463d8d96386c5352a6a20d7bbabf6c02827
eniolakura <enybeats76@gmail.com>
2022-09-14 06:45:51 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of a number
+ *
+ * @i: integer being converted
+ *
+ * Return: absolute value of integer
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+		return (i);
+	}
+}
